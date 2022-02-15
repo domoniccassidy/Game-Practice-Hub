89e4f81714f3f0859bbcfe0e567d29eca92b996e
Domonic Cassidy <domoniccassidy20@gmail.com>
2022-02-15 17:41:49 +0000

    cunt

--- Game Practice Hub/Assets/Scripts/ArenaPlayer.cs
+++ Game Practice Hub/Assets/Scripts/ArenaPlayer.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ArenaPlayer : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
@@ -12,8 +12,8 @@ public class ArenaPlayer : MonoBehaviour
     private int currentHealth;
     private float horizontalInput;
     private float verticalInput;
-    
-
+    private bool knockback;
+    private float knockbackStart;
 
     // Update is called once per frame
     private void Start()
@@ -23,14 +23,26 @@ public class ArenaPlayer : MonoBehaviour
     }
     void Update()
     {
+        if (knockback) 
+        {
+            knockbackStart -= Time.deltaTime;
+            //transform.position += -transform.forward * movementSpeed * 3 * Time.deltaTime;
+            //transform.position += transform.up * movementSpeed * 3 * Time.deltaTime;
 
+
+            if (knockbackStart <= 0)
+            {
+                knockback = false;
+            }
+            
+        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position +=  (transform.forward   *  movementSpeed);
+            transform.position +=  (transform.forward   *  movementSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= (transform.forward * movementSpeed);
+            transform.position -= (transform.forward * movementSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
@@ -40,12 +52,60 @@ public class ArenaPlayer : MonoBehaviour
         {
             transform.Rotate(-rotationSpeed);
         }
+        if (Input.GetKey(KeyCode.R))
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            GetComponent<MeshRenderer>().material.color = Color.green;
+
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+
+        }
+
 
     }
     public void Damage(int damage)
     {
+  
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        if (currentHealth == 0)
+        {
+            Die();
+        }
     }
-    
+    public void Knockback(Vector3 e, int knockbackForce)
+    {
+        knockback = true;
+        knockbackStart = 0.8f;
+
+        GetComponent<Rigidbody>().AddForce((e + new Vector3(-0.2f,1f)) * knockbackForce);
+
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+
+        enemy.SetActivated(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Enemy enemy = other.GetComponentInParent<Enemy>();
+
+        enemy.SetActivated(false);
+    }
+
 }
--- Game Practice Hub/Assets/Scripts/Enemy.cs
+++ Game Practice Hub/Assets/Scripts/Enemy.cs
@@ -8,21 +8,39 @@ public class Enemy : MonoBehaviour
     public int damage;
     public Transform Player;
     public ArenaPlayer playerScript;
+    public int knockbackForce;
+
+    bool activated = false;
+    Vector3 dirToPlayer = new Vector3(0, 0, 0);
     // Update is called once per frame
     void Update()
     {
         
-        Vector3 dirToPlayer = (Player.position - transform.position).normalized;
-        transform.position += dirToPlayer * MovementSpeed * Time.deltaTime;
+         dirToPlayer = (Player.position - transform.position).normalized;
+        if (activated)
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
+            transform.position += dirToPlayer * MovementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.black;
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "TankContainer")
+        if (collision.gameObject.name == "Player1")
         {
+            playerScript.Knockback(dirToPlayer,knockbackForce);
             playerScript.Damage(damage);
         }
     }
 
+    public void SetActivated(bool activated)
+    {
+        this.activated = activated;
+    }
 
 }
